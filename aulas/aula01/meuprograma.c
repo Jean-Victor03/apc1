bdfe6164905e9fc2155cdfb693e3de1090f5c0b4
Jean-Victor03 <142408704+Jean-Victor03@users.noreply.github.com>
2023-08-23 13:57:01 +0000

    envio para o github

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+// funcao principal
+int main() {
+  printf("Ola Pesoal!\n");
+  printf("Raiz qudrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
---
+++ praticas/pratica01/meuprogram01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  float nota_a1 = 3.9f;
+  float nota_a2 = 8.0f;
+
+   float media = 0.4 * nota_a1 + 0.6 * nota_a2;
+
+   printf("A média é %f\n", media);
+     
+  return 0;
+}
